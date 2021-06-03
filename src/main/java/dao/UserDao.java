3ba16bb422897d83845565fa2b565f4dd3d5c8c2
pika96 <52442949+pika96@users.noreply.github.com>
2021-06-03 16:41:20 +0900

    add 전략 패턴 적용

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -19,16 +19,8 @@ public class UserDao {
      * User 추가
      */
     public void add(User user) throws ClassNotFoundException, SQLException {
-
-        Connection c = connectionMaker.makeNewConnection();
-        PreparedStatement ps = c.prepareStatement(
-                "insert into users(id, name, password) values(?,?,?)");
-        ps.setString(1, user.getId());
-        ps.setString(2, user.getName());
-        ps.setString(3, user.getPassword());
-        ps.executeUpdate();
-        ps.close();
-        c.close();
+        StatementStrategy strategy = new AddStatement(user);
+        jdbcContextWithStatementStrategy(strategy);
     }
 
     /*
