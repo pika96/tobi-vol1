ea215f2737fba931abd841c64e5647b402b971dc
pika96 <52442949+pika96@users.noreply.github.com>
2021-06-05 20:48:40 +0900

    제네릭 사용

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -7,8 +7,8 @@ import java.io.IOException;
 public class Calculator {
 
     public Integer calcSum(String numFilepath) throws IOException {
-        LineCallback lineCallback =
-                new LineCallback() {
+        LineCallback<Integer> lineCallback =
+                new LineCallback<Integer>() {
                     @Override
                     public Integer doSomethingWithLine(String line, Integer value) {
                         return value + Integer.valueOf(line);
@@ -19,8 +19,8 @@ public class Calculator {
     }
 
     public Integer calcMultiply(String numFilepath) throws IOException {
-        LineCallback lineCallback =
-                new LineCallback() {
+        LineCallback<Integer> lineCallback =
+                new LineCallback<Integer>() {
                     @Override
                     public Integer doSomethingWithLine(String line, Integer value) {
                         return value * Integer.valueOf(line);
@@ -49,11 +49,11 @@ public class Calculator {
         }
     }
 
-    public Integer lineReadTemplate(String filepath, LineCallback callback, int initVal) throws  IOException {
+    public <T> T lineReadTemplate(String filepath, LineCallback<T> callback, T initVal) throws  IOException {
         BufferedReader br = null;
         try {
             br = new BufferedReader(new FileReader(filepath));
-            Integer res = initVal;
+            T res = initVal;
             String line = null;
             while((line = br.readLine()) != null) {
                 res = callback.doSomethingWithLine(line, res);
