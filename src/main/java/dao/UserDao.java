c23c164f22f9cd4a8c809933a93f59bf9d016a3a
pika96 <52442949+pika96@users.noreply.github.com>
2021-06-06 14:35:29 +0900

    jdbcTemplate로 변경

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -1,6 +1,8 @@
 package dao;
 
 import domain.User;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -10,98 +12,48 @@ import java.sql.SQLException;
 
 public class UserDao {
 
-    private JdbcContext jdbcContext;
+    private JdbcTemplate jdbcTemplate;
 
     public UserDao(DataSource dataSource) {
-        this.jdbcContext = new JdbcContext(dataSource);
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
     /*
      * User 추가
      */
-    public void add(final User user) throws ClassNotFoundException, SQLException {
-        this.jdbcContext.executeSql("insert into users(id, name, password) values(?,?,?)",
+    public void add(final User user) {
+        this.jdbcTemplate.update("insert into users(id, name, password) values(?,?,?)",
                 user.getId(), user.getName(), user.getPassword());
     }
 
     /*
      * User 정보 가져오기
      */
-    public User get(String id) throws ClassNotFoundException, SQLException {
-
-        Connection c = this.jdbcContext.getDataSource().getConnection();
-        PreparedStatement ps = c.prepareStatement(
-                "select * from users where id = ?");
-        ps.setString(1, id);
-        ResultSet rs = ps.executeQuery();
-        rs.next();
-        User user = new User();
-        user.setId(rs.getString("id"));
-        user.setName(rs.getString("name"));
-        user.setPassword(rs.getString("password"));
-        rs.close();
-        ps.close();
-        c.close();
-        return user;
+    public User get(String id) {
+        return this.jdbcTemplate.queryForObject("select * from users where id = ?",
+                (rs, rn) -> new User(
+                        rs.getString("id"),
+                        rs.getString("name"),
+                        rs.getString("password")
+                )
+                ,id);
     }
 
     /*
      * User 삭제
      */
-    public void delete(String id) throws ClassNotFoundException, SQLException {
-
-        Connection c = this.jdbcContext.getDataSource().getConnection();
-        PreparedStatement ps = c.prepareStatement(
-                "delete from users where id = ?");
-        ps.setString(1, id);
-        ps.executeUpdate();
-        ps.close();
-        c.close();
+    public void delete(String id) {
+        this.jdbcTemplate.update("delete from users where id = ?");
     }
 
     /*
      * User 정보 초기화
      */
-    public void deleteAll() throws ClassNotFoundException, SQLException {
-        this.jdbcContext.executeSql("delete from users");
+    public void deleteAll() {
+        this.jdbcTemplate.update("delete from users");
     }
 
-    public int getCount() throws ClassNotFoundException, SQLException {
-        Connection c = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-
-        try {
-            c = this.jdbcContext.getDataSource().getConnection();
-            ps = c.prepareStatement("select count(*) from users");
-
-            rs = ps.executeQuery();
-            rs.next();
-            return rs.getInt(1);
-        } catch (SQLException e) {
-            throw e;
-        } finally {
-            if (rs != null) {
-                try {
-                    rs.close();
-                } catch (SQLException e) {
-
-                }
-            }
-            if (ps != null) {
-                try {
-                    ps.close();
-                } catch (SQLException e) {
-
-                }
-            }
-            if (c != null) {
-                try {
-                    c.close();
-                } catch (SQLException e) {
-
-                }
-            }
-        }
+    public int getCount() {
+        return this.jdbcTemplate.queryForObject("select count(*) from users", Integer.class);
     }
 }
